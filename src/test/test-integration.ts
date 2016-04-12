c1ab810ce8668f1b065b24d55b1aa0ce14240f33
Lucas Marquez <lucasasecas@gmail.com>
2016-04-12 14:53:36 -0300

    create tests for one clinet

--- src/test/test-integration.ts
+++ src/test/test-integration.ts
@@ -36,4 +36,99 @@ describe("Integration test between client and service", function(){
   }
 }
 
+  it("Test with one client, case one: change, client tic, change server tic.", function(){
+      // Arrange
+        // Prepare variables
+      var boardVM: BoardVM = new BoardVM();
+      var client = new App(boardVM);
+      var noteVM = boardVM.createNote();
+      var beginingText = "This is the begining part of the test";
+      var endingText = " and this is the ending part of the test";
+      var boardService = new BoardService();
+      var serverMock = new ServerMock(boardService);
+      serverMock.addClient(client);
+
+      // Act
+      noteVM.content(beginingText);
+      client.onInterval();
+      noteVM.content(noteVM.content() + endingText);
+      boardService.onTic();
+
+      //Assert
+      expect(noteVM.content()).toEqual(beginingText + endingText);
+    });
+
+  it("Test with one client, case two: change, client tic, change, client tic, server tic.", function(){
+      // Arrange
+        // Prepare variables
+      var boardVM: BoardVM = new BoardVM();
+      var client = new App(boardVM);
+      var noteVM = boardVM.createNote();
+      var beginingText = "This is the begining part of the test";
+      var endingText = " and this is the ending part of the test";
+      var boardService = new BoardService();
+      var serverMock = new ServerMock(boardService);
+      serverMock.addClient(client);
+
+      // Act
+      noteVM.content(beginingText);
+      client.onInterval();
+      noteVM.content(noteVM.content() + endingText);
+      client.onInterval();
+      boardService.onTic();
+
+      //Assert
+      expect(noteVM.content()).toEqual(beginingText + endingText);
+    });
+
+  it("Test with one client, case three: change, client tic, server tic, change, client tic, server tic.", function(){
+      // Arrange
+        // Prepare variables
+      var boardVM: BoardVM = new BoardVM();
+      var client = new App(boardVM);
+      var noteVM = boardVM.createNote();
+      var beginingText = "This is the begining part of the test";
+      var endingText = " and this is the ending part of the test";
+      var boardService = new BoardService();
+      var serverMock = new ServerMock(boardService);
+      serverMock.addClient(client);
+
+      // Act
+      noteVM.content(beginingText);
+      client.onInterval();
+      boardService.onTic();
+      noteVM.content(noteVM.content() + endingText);
+      client.onInterval();
+    boardService.onTic();
+
+      //Assert
+      expect(noteVM.content()).toEqual(beginingText + endingText);
+    });
+
+
+  it("Test with one client, case four: change, client tic, server tic, change, client tic, server tic", function(){
+      // Arrange
+        // Prepare variables
+      var boardVM: BoardVM = new BoardVM();
+      var client = new App(boardVM);
+      var noteVM = boardVM.createNote();
+      var beginingText = "This is the begining part of the test";
+      var endingText = " and this is the ending part of the test";
+      var boardService = new BoardService();
+      var serverMock = new ServerMock(boardService);
+      serverMock.addClient(client);
+
+      // Act
+      noteVM.content(beginingText);
+      boardService.onTic();
+      client.onInterval();
+      boardService.onTic();
+      noteVM.content(noteVM.content() + endingText);
+      client.onInterval();
+      boardService.onTic();
+
+      //Assert
+      expect(noteVM.content()).toEqual(beginingText + endingText);
+    });
+
 });
